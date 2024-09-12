9c89fd5ff39dffe73d3ea4c64873e50a3fb263fb
Dinislam <axmetshin.dinislam@mail.ru>
2024-09-12 15:54:43 +0500

    Желтый фон и кнопка закрыть

---
+++ 1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Блакнот : Form
+    {
+        public Блакнот()
+        {
+            InitializeComponent();
+        }
+
+        private void Закрыть_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
